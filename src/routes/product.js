5550ebec23cf78f01d3ccdff1b7eec81b3694785
MedChouk <chouk.med20@gmail.com>
2022-01-25 21:05:53 +0100

    upload img product with route

--- src/routes/product.js
+++ src/routes/product.js
@@ -4,11 +4,24 @@ const express = require('express');
 const { requireSignin, adminMiddleware } = require('../common-middleware');
 const { createProduct,  } = require('../controller/product');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const shortid = require('shortid');
+const path = require('path');
+
 
 const router = express.Router();
 
-router.post('/product/create', requireSignin, adminMiddleware, upload.single('productPicture'), createProduct);
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(path.dirname(__dirname), 'uploads'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, shortid.generate() + '-' + file.originalname)
+    }
+  })
+
+const upload = multer({ storage });
+
+router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture'), createProduct);
 
 
 // router.get('/category/getcategory', getCategories);
