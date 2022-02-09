6b7b09da95c350374cb19a5ad8939e4d62d4832f
MedChouk <chouk.med20@gmail.com>
2022-02-09 19:05:29 +0100

    add initialdata

--- src/index.server.js
+++ src/index.server.js
@@ -11,6 +11,7 @@ const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
 const cartRoutes = require('./routes/cart');
+const initialDataRoutes = require("./routes/admin/initialData");
 
 const app = express();
 
@@ -39,6 +40,7 @@ app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 app.use('/api', cartRoutes);
+app.use("/api", initialDataRoutes);
 
 
 //test route : 
