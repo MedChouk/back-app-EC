9e7efda7c9411d0d0832ce8de1db57afe7f0b321
MedChouk <chouk.med20@gmail.com>
2022-02-12 21:49:44 +0100

    Improvement in auth backend

--- src/controller/admin/auth.js
+++ src/controller/admin/auth.js
@@ -1,6 +1,7 @@
 const User = require('../../models/user');
 const jwt = require('jsonwebtoken');
 const bcrypt = require ('bcrypt');
+const shortid = require('shortid');
 
 exports.signup = (req, res) => {
 
@@ -21,7 +22,7 @@ exports.signup = (req, res) => {
             lastName,
             email,
             hash_password,
-            username: Math.random().toString(),
+            username: shortid.generate(),
             role: 'admin'
         });
 
