3b93b81e8974ffc74fa4dd30a0184af0256312d8
MedChouk <chouk.med20@gmail.com>
2022-02-15 20:37:04 +0100

    fetch product api

--- src/controller/product.js
+++ src/controller/product.js
@@ -1,40 +1,83 @@
-const Product = require('../models/product');
-const shortid = require('shortid');
-const slugify = require('slugify');
+const Product = require("../models/product");
+const shortid = require("shortid");
+const slugify = require("slugify");
+const Category = require("../models/category");
 
 exports.createProduct = (req, res) => {
+	//res.status(200).json({file: req.files, body: req.body })
 
-    //res.status(200).json({file: req.files, body: req.body })
+	const { name, price, description, category, quantity, createdBy } = req.body;
 
-    const {
-        name, price, description, category, quantity, createdBy
-    } = req.body;
+	let productPictures = [];
 
-    let productPictures = [];
+	if (req.files.length > 0) {
+		productPictures = req.files.map((file) => {
+			return { img: file.filename };
+		});
+	}
 
-    if(req.files.length > 0){
-        productPictures = req.files.map(file => {
-            return { img: file.filename }
-        });
-    }
+	const product = new Product({
+		name: name,
+		slug: slugify(name),
+		price,
+		description,
+		quantity,
+		productPictures,
+		category,
+		createdBy: req.user._id,
+	});
 
-    const product = new Product({
-        name: name,
-        slug: slugify(name),
-        price,
-        description,
-        quantity,
-        productPictures,
-        category,
-        createdBy: req.user._id
+	product.save((error, product) => {
+		if (error) return res.status(400).json({ error });
+		if (product) {
+			res.status(201).json({ product });
+		}
+	});
+};
 
-    });
+exports.getProductsBySlug = (req, res) => {
+	const { slug } = req.params;
+	Category.findOne({ slug: slug })
+		.select("_id")
+		.exec((error, category) => {
+			if (error) {
+				return res.status(400).json({ error });
+			}
 
-    product.save(((error, product) => {
-        if(error) return res.status(400).json({ error });
-        if (product){
-           res.status(201).json({ product });
-        }
-    }));
+			if (category) {
+				Product.find({ category: category._id }).exec((error, products) => {
+					if (error) {
+						return res.status(400).json({ error });
+					}
 
+					if (products.length > 0) {
+						res.status(200).json({
+							products,
+							priceRange: {
+								under5k: 500,
+								under10k: 1000,
+								under15k: 1500,
+								under20k: 2000,
+								under30k: 3000,
+							},
+							productsByPrice: {
+								under5k: products.filter((product) => product.price <= 500),
+								under10k: products.filter(
+									(product) => product.price > 500 && product.price <= 1000
+								),
+								under15k: products.filter(
+									(product) => product.price > 1000 && product.price <= 1500
+								),
+								under20k: products.filter(
+									(product) => product.price > 1500 && product.price <= 2000
+								),
+								under30k: products.filter(
+									(product) => product.price > 2000 && product.price <= 30000
+								),
+							},
+						});
+					}
+				});
+			}
+		});
 };
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const { addCategory, getCategories } = require('../controller/category');
 
 const { requireSignin, adminMiddleware } = require('../common-middleware');
-const { createProduct,  } = require('../controller/product');
+const { createProduct, getProductsBySlug,  } = require('../controller/product');
 const multer = require('multer');
 const shortid = require('shortid');
 const path = require('path');
@@ -22,6 +22,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture'), createProduct);
+router.get("/products/:slug", getProductsBySlug);
 
 
 // router.get('/category/getcategory', getCategories);
