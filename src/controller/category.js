3a575d536d070656dfb04bce577a4028f36d02ed
MedChouk <chouk.med20@gmail.com>
2022-02-19 18:12:47 +0100

    add updateCategories

--- src/controller/category.js
+++ src/controller/category.js
@@ -4,14 +4,14 @@ const slugify = require('slugify');
 function createCategories(categories, parentId = null) {
     const categoryList = [];
     let category;
-    if (parentId ==null) {
+    if (parentId == null) {
         category = categories.filter(cat => cat.parentId == undefined);
     } else {
         category = categories.filter(cat => cat.parentId == parentId);
     }
 
-    for(let cate of category){
-        categoryList.push({ 
+    for (let cate of category) {
+        categoryList.push({
             _id: cate._id,
             name: cate.name,
             slug: cate.slug,
@@ -19,41 +19,76 @@ function createCategories(categories, parentId = null) {
             children: createCategories(categories, cate._id)
         });
     }
-
     return categoryList;
 }
 
 exports.addCategory = (req, res) => {
+    const categoryObj = {
+        name: req.body.name,
+        slug: slugify(req.body.name)
+    }
 
-        const categoryObj = {
-            name: req.body.name,
-            slug: slugify(req.body.name)
-        }
+    if (req.file) {
+        categoryObj.categoryImage = process.env.API + '/public/' + req.file.filename;
+    }
 
-        if (req.file) {
-            categoryObj.categoryImage = process.env.API + '/public/' + req.file.filename;
-        }
-    
-        if(req.body.parentId) {
-            categoryObj.parentId = req.body.parentId
-        }
-    
-        const cat = new Category(categoryObj);
-        cat.save((error, category) => {
-            if(error) return res.status(400).json({ error });
-            if(category) {return res.status(201).json({ category });}
-        });
-   
+    if (req.body.parentId) {
+        categoryObj.parentId = req.body.parentId
+    }
+
+    const cat = new Category(categoryObj);
+    cat.save((error, category) => {
+        if (error) return res.status(400).json({ error });
+        if (category) { return res.status(201).json({ category }); }
+    });
+
 }
 
 exports.getCategories = (req, res) => {
 
-        Category.find({})
+    Category.find({})
         .exec((error, categories) => {
-            if(error) return res.status(400).json({ error });
-            if(categories) {
+            if (error) return res.status(400).json({ error });
+            if (categories) {
                 const categoryList = createCategories(categories);
-                res.status(200).json({ categoryList });}
+                res.status(200).json({ categoryList });
+            }
         });
-   
-} 
+
+}
+
+exports.updateCategories = async (req, res) => {
+    const { _id, name, parentId, type } = req.body;
+    const updatedCategories = [];
+    if (name instanceof Array) {
+        for (let i = 0; i < name.length; i++) {
+            const category = {
+                name: name[i],
+                type: type[i],
+            };
+            if (parentId[i] !== "") {
+                category.parentId = parentId[i];
+            }
+
+            const updatedCategory = await Category.findOneAndUpdate(
+                { _id: _id[i] },
+                category,
+                { new: true }
+            );
+            updatedCategories.push(updatedCategory);
+        }
+        return res.status(201).json({ updateCategories: updatedCategories });
+    } else {
+        const category = {
+            name,
+            type,
+        };
+        if (parentId !== "") {
+            category.parentId = parentId;
+        }
+        const updatedCategory = await Category.findOneAndUpdate({ _id }, category, {
+            new: true,
+        });
+        return res.status(201).json({ updatedCategory });
+    }
+};
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addCategory, getCategories } = require('../controller/category');
+const { addCategory, getCategories, updateCategories } = require('../controller/category');
 const { requireSignin, adminMiddleware } = require('../common-middleware');
 const multer = require('multer');
 const shortid = require('shortid');
@@ -9,18 +9,19 @@ const path = require('path');
 const router = express.Router();
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(path.dirname(__dirname), 'uploads'))
-    },
-    filename: function (req, file, cb) {
-      cb(null, shortid.generate() + '-' + file.originalname)
-    }
-  })
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), 'uploads'))
+  },
+  filename: function (req, file, cb) {
+    cb(null, shortid.generate() + '-' + file.originalname)
+  }
+})
 
 const upload = multer({ storage });
 
 
 router.post('/category/create', requireSignin, adminMiddleware, upload.single('categoryImage'), addCategory);
 router.get('/category/getcategory', getCategories);
+router.post('/category/update', upload.array('categoryImage'), updateCategories);
 
 module.exports = router;
