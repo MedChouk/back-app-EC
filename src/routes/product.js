06ce4f2af0e1dc48c1d188121ebc29a826a12194
MedChouk <chouk.med20@gmail.com>
2023-02-16 22:56:43 +0100

    User Login And Product Page

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const { addCategory, getCategories } = require('../controller/category');
 
 const { requireSignin, adminMiddleware } = require('../common-middleware');
-const { createProduct, getProductsBySlug,  } = require('../controller/product');
+const { createProduct, getProductsBySlug, getProductDetailsById } = require('../controller/product');
 const multer = require('multer');
 const shortid = require('shortid');
 const path = require('path');
@@ -23,6 +23,7 @@ const upload = multer({ storage });
 
 router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture'), createProduct);
 router.get("/products/:slug", getProductsBySlug);
+router.get("/product/:productId", getProductDetailsById);
 
 
 // router.get('/category/getcategory', getCategories);
